b7c56f4626a0a98cb4a78adbcc86802ae464fc46
Roman-Lahoda <lagoda9515@gmail.com>
2021-05-18 02:30:00 +0300

    Update menu-test.js

--- src/menu-test.js
+++ src/menu-test.js
@@ -6,46 +6,19 @@
   const menuBtn5Ref = document.querySelector("[item3]")
   const menuBtn6Ref = document.querySelector("[item4]")
   const mobileMenuRef = document.querySelector("[data-menu]")
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
-    document.body.classList.toggle("menu-open")
-    menuBtnRef.setAttribute("aria-expanded", !expanded)
-    mobileMenuRef.classList.toggle("is-open")
-  })
+  menuBtnRef.addEventListener("click", menu);
+  menuBtn2Ref.addEventListener("click", menu);
+  menuBtn3Ref.addEventListener("click", menu);
+  menuBtn4Ref.addEventListener("click", menu);
+  menuBtn5Ref.addEventListener("click", menu);
+  menuBtn6Ref.addEventListener("click", menu);
 
-  menuBtn2Ref.addEventListener("click", () => {
+  function menu() {
     const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
     document.body.classList.toggle("menu-open")
     menuBtn2Ref.setAttribute("aria-expanded", !expanded)
     mobileMenuRef.classList.toggle("is-open")
-  })
-
-  menuBtn3Ref.addEventListener("click", () => {
-    const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
-    document.body.classList.toggle("menu-open")
-    menuBtn2Ref.setAttribute("aria-expanded", !expanded)
-    mobileMenuRef.classList.toggle("is-open")
-  })
-
-  menuBtn4Ref.addEventListener("click", () => {
-    const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
-    document.body.classList.toggle("menu-open")
-    menuBtn2Ref.setAttribute("aria-expanded", !expanded)
-    mobileMenuRef.classList.toggle("is-open")
-  })
-
-  menuBtn5Ref.addEventListener("click", () => {
-    const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
-    document.body.classList.toggle("menu-open")
-    menuBtn2Ref.setAttribute("aria-expanded", !expanded)
-    mobileMenuRef.classList.toggle("is-open")
-  })
-
-  menuBtn6Ref.addEventListener("click", () => {
-    const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
-    document.body.classList.toggle("menu-open")
-    menuBtn2Ref.setAttribute("aria-expanded", !expanded)
-    mobileMenuRef.classList.toggle("is-open")
-  })
+  }
+  
 })()
 

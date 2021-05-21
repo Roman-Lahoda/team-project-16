eb77bef20352f6e7f6b542f69d2ed30001a448c8
Roman-Lahoda <lagoda9515@gmail.com>
2021-05-21 16:33:26 +0300

    change

--- src/modal.js
+++ src/modal.js
@@ -1,16 +1,11 @@
 ;(() => {
     const modalBtnRef = document.querySelector("[data-modal-open]")
     const modalBtn2Ref = document.querySelector("[data-modal-close]")
-    const modalBtn3Ref = document.querySelector("[data-modal-close2]")
     const modalRef = document.querySelector("[data-modal]")
     modalBtnRef.addEventListener("click", menu);
     modalBtn2Ref.addEventListener("click", menu);
-    modalBtn3Ref.addEventListener("click", menu);
-    // modalRef.addEventListener("click", menu);
     function menu() {
-    //   const expanded = menuBtn2Ref.getAttribute("aria-expanded") === "true" || false
       document.body.classList.toggle("modal-open")
-    //   menuBtn2Ref.setAttribute("aria-expanded", !expanded)
       modalRef.classList.toggle("hidden")
     }
   })()

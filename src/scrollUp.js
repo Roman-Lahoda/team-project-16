9a7add8f389ab11aa726804cfc3ffb63cc9c24a2
Natalia Pavlova <pavlova_li@ukr.net>
2021-05-30 19:29:26 +0300

    arrow

--- src/scrollUp.js
+++ src/scrollUp.js
@@ -1,34 +1,88 @@
-// We select the element we want to target
-var target = document.querySelector(".registration");
-
-
-var scrollToTopBtn = document.querySelector(".scrollup")
-var rootElement = document.documentElement
-
-// Next we want to create a function that will be called when that element is intersected
-function callback(entries, observer) {
-  // The callback will return an array of entries, even if you are only observing a single item
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // Show button
-      scrollToTopBtn.classList.add("showBtn")
+/*$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $(".scrollup").fadeIn();
     } else {
-      // Hide button
-      scrollToTopBtn.classList.remove("showBtn")
+      $(".scrollup").fadeOut();
     }
   });
+
+      $(".scrollup").click(function () {
+  $("body,html").animate({ scrollTop: 0 }, 800);
+      });
+});*/
+    
+
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('.scrollup').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop:0
+    },1000);
+  })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop()>200) {
+    // то сделать кнопку scrollup видимой
+    $('.scrollup').fadeIn();
+  }
+  // иначе скрыть кнопку scrollup
+  else {
+    $('.scrollup').fadeOut();
+  }
+});
+
+/*// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById(".scrollup").style.display = "block";
+    } else {
+        document.getElementById(".scrollup").style.display = "none";
+    }
 }
 
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
-    
-// Next we instantiate the observer with the function we created above. This takes an optional configuration
-// object that we will use in the other examples.
-let observer = new IntersectionObserver(callback);
-// Finally start observing the target element
-observer.observe(target);
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
+
+/*$(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('.scrollup').show();
+		} else {
+			$('.scrollup').hide();
+		}
+	});
+ 
+	$('.scrollup').click(function(){
+		$('html, body').animate({scrollTop: 0}, 600);
+		return false;
+	});
+});*/
+
+
+/*$(function() {
+  $.fn.scrollToTop = function() {
+   $(this).hide().removeAttr("href");
+   if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+   var scrollDiv = $(this);
+   $(window).scroll(function() {
+    if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+    else $(scrollDiv).fadeIn("slow")
+   });
+   $(this).click(function() {
+    $("html, body").animate({scrollTop: 0}, "slow")
+   })
+  }
+});
+   
+$(function() {
+  $(".scrollup").scrollToTop();
+});*/
